45aeb92d7a402adc58cc8e937624f684c5836140
NovaCygni <novacygni@hotmail.co.uk>
2017-11-10 14:30:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='V3n0M',
-      version='421',
-      description="Popular SQLi and Pentesting scanner in Python 3.6",
-      author='NovaCygni',
-      author_email='404 Not Found',
-      url='https://github.com/v3n0m-Scanner/V3n0M-Scanner',
-      package_dir={'v3n0m': 'src'},
-      packages=['v3n0m'], install_requires=['aiohttp', 'httplib2', 'socksipy-branch', 'requests', 'url', 'bs4',
-                                            'pip', 'dnspython', 'tqdm', 'aioftp', 'termcolor']
-      )
+# noinspection PyBroadException
+try:
+    try:
+        setup(name='V3n0M',
+              version='422',
+            description="Popular SQLi and Pentesting scanner in Python 3.6",
+            author='NovaCygni',
+            author_email='404 Not Found',
+            url='https://github.com/v3n0m-Scanner/V3n0M-Scanner',
+            package_dir={'v3n0m': 'src'},
+            packages=['v3n0m'], requires=['aiohttp', 'httplib2', 'socksipy-branch', 'requests', 'url', 'bs4',
+                                        'pip', 'dnspython', 'tqdm', 'aioftp', 'termcolor'])
+    except:
+        from setuptools import setup
+
+        setup(name='V3n0M',
+              version='422',
+            description="Popular SQLi and Pentesting scanner in Python 3.6",
+            author='NovaCygni',
+            author_email='404 Not Found',
+            url='https://github.com/v3n0m-Scanner/V3n0M-Scanner',
+            package_dir={'v3n0m': 'src'},
+            packages=['v3n0m'], install_requires=['aiohttp', 'httplib2', 'socksipy-branch', 'requests', 'url', 'bs4',
+                                                'pip', 'dnspython', 'tqdm', 'aioftp', 'termcolor'])
+except Exception:
+    print(Exception)
