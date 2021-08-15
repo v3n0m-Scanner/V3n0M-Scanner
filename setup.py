8d67e3a772bd655cca65ae80b2bc8ec0d0f297c1
0xArchitect <vittring@riseup.net>
2021-08-14 22:26:35 -0400

    Minor setup.py fixes. Add proper .gitignore for near future tests and docs. Finalized setup.py which wasn't working entirely correctly.

--- setup.py
+++ setup.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python
-from distutils.core import setup
 
-# noinspection PyBroadException
-try:
-    try:
-        setup(name='V3n0M',
-              version='425',
-            description="Popular SQLi and Pentesting scanner in Python 3.6",
-            author='NovaCygni',
-            author_email='404 Not Found',
-            url='https://github.com/v3n0m-Scanner/V3n0M-Scanner',
-            package_dir={'v3n0m': 'src'},
-            packages=['v3n0m'])
-    except:
-        from setuptools import setup
+from setuptools import setup
 
-        setup(name='V3n0M',
-            version='425',
-            description="Popular SQLi and Pentesting scanner in Python 3.6",
-            author='NovaCygni',
-            author_email='404 Not Found',
-            url='https://github.com/v3n0m-Scanner/V3n0M-Scanner',
-            package_dir={'v3n0m': 'src'},
-            packages=['v3n0m'])
-        
-except Exception as verb:
-    print(str(verb))
+with open("README.md", "r") as descript:
+    long_describe = descript.read()
+
+setup(
+    name='venom',
+    version='425',
+    description="Tool for Vulnerability Scanning & Pentesting",
+    long_description=long_describe,
+    long_description_content_type="text/markdown",
+    author='NovaCygni',
+    author_email='404 Not Found',
+    url='https://github.com/v3n0m-Scanner/V3n0M-Scanner',
+    package_dir={'v3n0m': 'src'},
+    packages=['v3n0m']
+)
